acb921e60161a2f9d04970493614311f6b1446ec
Mohssine El Aini <mel-aini@e3r8p1.1337.ma>
2023-08-25 08:41:02 +0100

    Animate brands section

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -4,16 +4,37 @@ import { ReactComponent as Logo3 } from '../assets/brands/logo-2.svg';
 import { ReactComponent as Logo4 } from '../assets/brands/logo-3.svg';
 import { ReactComponent as Logo5 } from '../assets/brands/logo-4.svg';
 import { ReactComponent as Logo6 } from '../assets/brands/logo-5.svg';
+import { useEffect, useRef, useState } from 'react';
 
 function Brands() {
+	const [leftP, setLeftP] = useState('0px');
+	const child = useRef();
+	const parrent = useRef();
+
+	useEffect(() => {
+		let	x1 = parseInt(getComputedStyle(child.current).getPropertyValue('left'));
+		const id = setInterval(() => {
+			if (x1 === -child.current.offsetWidth) {
+				setLeftP(parrent.current.offsetWidth + 'px');
+				x1 = parrent.current.offsetWidth;
+			}
+			else {
+				x1--;
+				setLeftP(x1 + 'px');
+			}
+		}, 10);
+	}, []);
+	
 	return (
-		<div className="flex overflow-hidden">
-			<Logo1 className="mx-[30px]"/>
-			<Logo2 className="mx-[30px]"/>
-			<Logo3 className="mx-[30px]"/>
-			<Logo4 className="mx-[30px]"/>
-			<Logo5 className="mx-[30px]"/>
-			<Logo6 className="mx-[30px]"/>
+		<div ref={ parrent } className="flex overflow-hidden relative w-[80%] py-[50px]">
+			<div  ref={ child } className="flex overflow-hidden absolute" style={{left: leftP}}>
+				<Logo1 className="mx-[30px] lg:mx-[80px]"/>
+				<Logo2 className="mx-[30px] lg:mx-[80px]"/>
+				<Logo3 className="mx-[30px] lg:mx-[80px]"/>
+				<Logo4 className="mx-[30px] lg:mx-[80px]"/>
+				<Logo5 className="mx-[30px] lg:mx-[80px]"/>
+				<Logo6 className="mx-[30px] lg:mx-[80px]"/>
+			</div>
 		</div>
 	);
 }
