9d2782c9ccbf2cc3737cbf2eb7a5b8149be3520f
mel-aini <elaini.mohssine@gmail.com>
2023-09-15 18:00:07 +0100

    Hovering in services buttons animation

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,5 +1,12 @@
+import Aat from "../assets/services/Aat.png";
+import Cc from "../assets/services/Cc.png";
+import Em from "../assets/services/Em.png";
+import PpcA from "../assets/services/PpcA.png";
+import Seo from "../assets/services/Seo.png";
+import Smm from "../assets/services/Smm.png";
+
 import { useState, useEffect } from "react";
-import ServicesItems from "./ServicesItems";
+import ServicesItem from "./ServicesItem";
 import { ReactComponent as Illustration } from "../assets/services/Illustration.svg";
 import ReusableButton from "./ReusableButton";
 import PropTypes from "prop-types";
@@ -10,6 +17,70 @@ Services.propTypes = {
 
 function Services({ colors }) {
   const [smallW, setSmallW] = useState(true);
+  const blackColor = "#000000";
+
+  const services = [
+    {
+      title1: "Search engine",
+      title2: "optimization",
+      src: Seo,
+      bgColor: colors.main,
+      hBgColor: colors.main,
+      hColor: blackColor,
+      bBgColor: blackColor,
+      bColor: colors.main,
+    },
+    {
+      title1: "Pay-per-click",
+      title2: "advertising",
+      src: PpcA,
+      bgColor: colors.main,
+      hBgColor: colors.gray,
+      hColor: blackColor,
+      bBgColor: blackColor,
+      bColor: colors.main,
+    },
+    {
+      title1: "Social Media",
+      title2: "Marketing",
+      src: Smm,
+      bgColor: colors.dark,
+      hBgColor: colors.gray,
+      hColor: blackColor,
+      bBgColor: colors.gray,
+      bColor: blackColor,
+    },
+    {
+      title1: "Email",
+      title2: "Marketing",
+      src: Em,
+      bgColor: colors.gray,
+      hBgColor: colors.main,
+      hColor: blackColor,
+      bBgColor: blackColor,
+      bColor: colors.main,
+    },
+    {
+      title1: "Content",
+      title2: "Creation",
+      src: Cc,
+      bgColor: colors.main,
+      hBgColor: colors.gray,
+      hColor: blackColor,
+      bBgColor: blackColor,
+      bColor: colors.main,
+    },
+    {
+      title1: "Analytics and",
+      title2: "Tracking",
+      src: Aat,
+      bgColor: colors.dark,
+      hBgColor: colors.gray,
+      hColor: blackColor,
+      bBgColor: colors.gray,
+      bColor: blackColor,
+    },
+  ];
   useEffect(() => {
     window.innerWidth >= 1024 ? setSmallW(false) : setSmallW(true);
   }, []);
@@ -31,7 +102,12 @@ function Services({ colors }) {
           businesses grow and succeed online. These services include:
         </p>
       </div>
-      <ServicesItems colors={colors} />
+      {/* <ServicesItems colors={colors} /> */}
+      <div className="grid w-full gap-10 grid-cols-1 md:grid-cols-2 2xl:grid-cols-3">
+        {services.map((elem, index) => {
+          return <ServicesItem elem={elem} colors={colors} key={index + 1} />;
+        })}
+      </div>
       <div className="w-full bg-gray-200 h-[400px] flex justify-between gap-12 py-10 px-[10%] sm:px-12 mt-[80px] rounded-[45px]">
         <div className="flex flex-col items-start justify-between sm:px-[50px] md:py-[30px]">
           <h1 className="font-medium text-2xl sm:text-4xl">
---
+++ src/components/ServicesItem.jsx
@@ -0,0 +1,90 @@
+import { FaArrowLeft } from "react-icons/fa6";
+import PropTypes from "prop-types";
+import { useEffect, useRef, useState } from "react";
+
+ServicesItem.propTypes = {
+  elem: PropTypes.object,
+  colors: PropTypes.object,
+};
+
+function ServicesItem({ elem, colors }) {
+  const [scale, setScale] = useState(0);
+  const [btnScale, setBtnScale] = useState(1);
+  const [spanOpacity, setSpanOpacity] = useState(0);
+  const contRef = useRef(null);
+  const spanSize = 40;
+  const [spanPos, setSpanPos] = useState({ left: 0, bottom: 0 });
+
+  const getbgColor = () => {
+    if (elem.bgColor == colors.main) return colors.gray;
+    if (elem.bgColor == colors.dark) return colors.main;
+    if (elem.bgColor == colors.gray) return colors.main;
+  };
+  const spanBgColor = getbgColor();
+  useEffect(() => {
+    setSpanPos({
+      left: window.getComputedStyle(contRef.current).paddingLeft,
+      bottom: window.getComputedStyle(contRef.current).paddingBottom,
+    });
+  }, []);
+  return (
+    <div
+      ref={contRef}
+      style={{ backgroundColor: elem.bgColor }}
+      className="relative shadow-[0_8px] shadow-black w-full border-[1px] border-black border-solid flex flex-col items-center justify-center px-12 py-9 rounded-[45px] overflow-hidden"
+    >
+      <span
+        style={{
+          transform: `scale(${scale})`,
+          backgroundColor: spanBgColor,
+          width: spanSize,
+          height: spanSize,
+          left: spanPos.left,
+          bottom: spanPos.bottom,
+          opacity: spanOpacity,
+        }}
+        className="z-0 absolute bg-blue-500 rounded-full duration-500 ease-out"
+      ></span>
+      <h1
+        style={{ backgroundColor: elem.hBgColor, color: elem.hColor }}
+        className="z-10 self-start text-2xl px-[5px] font-medium rounded-lg"
+      >
+        {elem.title1}
+      </h1>
+      <h1
+        style={{ backgroundColor: elem.hBgColor, color: elem.hColor }}
+        className="z-10 self-start text-2xl px-[5px] font-medium bg-[#eee] rounded-lg"
+      >
+        {elem.title2}
+      </h1>
+      <div className="z-10 flex w-full justify-between items-end mt-12">
+        <div
+          style={{ transform: `scale(${btnScale})` }}
+          className="overflow-hidden w-[40px] h-[40px] p-[5px] cursor-pointer rounded-3xl relative duration-500 ease-out"
+        >
+          <FaArrowLeft
+            onMouseEnter={() => {
+              setScale(30);
+              setSpanOpacity(2);
+              setBtnScale(1.1);
+              console.log("hello");
+            }}
+            onMouseLeave={() => {
+              setScale(1);
+              setSpanOpacity(0);
+              setBtnScale(1);
+            }}
+            style={{
+              backgroundColor: elem.bBgColor,
+              color: elem.bColor,
+            }}
+            className="text-2xl rotate-[135deg] bg-[#eee] w-[40px] h-[40px] p-[5px] absolute top-0 left-0"
+          />
+        </div>
+        <img src={elem.src} />
+      </div>
+    </div>
+  );
+}
+
+export default ServicesItem;
